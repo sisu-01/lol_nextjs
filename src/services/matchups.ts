159e896d87f21f11824a016ea671277a1e116424
sisu-01 <tesv010615@naver.com>
2026-02-03 15:27:39 +0900

    매치업 불러오는 api

---
+++ src/services/matchups.ts
@@ -0,0 +1,23 @@
+import type { fetchedMatchupsType } from "../types/game";
+
+const API_URL = process.env.NEXT_API_URL;
+
+export const fetchMatchups = async (): Promise<fetchedMatchupsType | null> => {
+  try {
+    const endpoint = `${API_URL}/matchups`;
+    const response = await fetch(endpoint, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
