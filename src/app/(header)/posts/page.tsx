8bc46af4781db4847e387bc955c8b7cd7d5c3ee0
sisu-01 <tesv010615@naver.com>
2026-02-10 13:13:37 +0900

    post 목록 css

--- src/app/(header)/posts/page.tsx
+++ src/app/(header)/posts/page.tsx
@@ -14,7 +14,7 @@ const PostsPage = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 overflow-y-auto h-full">
       <div className="max-w-5xl mx-auto">
         
         {/* 헤더 섹션 */}
