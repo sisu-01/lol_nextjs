7a15054f6ca85a8a1f7a7fcf4fec8fadbfcb9704
sisu-01 <tesv010615@naver.com>
2026-03-01 15:56:12 +0900

    next->start

--- src/components/H5Adsense/H5Adsense.tsx
+++ src/components/H5Adsense/H5Adsense.tsx
@@ -13,7 +13,7 @@ const H5Adsense = () => {
       if (document.hidden) {
         // Google H5 Game Ads 호출 함수
         adBreak({
-          type: 'next', // 게임 외부 어쩌구 타입 광고
+          type: 'start', // 게임 외부 어쩌구 타입 광고
           name: 'tab-switch-pause',
           adBreakDone: (placementInfo) => {
             h5AdsenseDebug(placementInfo);
