9d3857f1525b194dbcc6077e087a5e2f8d96fe22
sisu-01 <tesv010615@naver.com>
2026-03-02 15:29:49 +0900

    하트 정렬

--- src/components/ingame/LifeBoard/LifeBoard.tsx
+++ src/components/ingame/LifeBoard/LifeBoard.tsx
@@ -11,9 +11,9 @@ const LifeBoard = ({ extraLife, rewardLife, addRewardLife }: lifeBoardProps) => 
   }
 
   return (
-    <div className="select-none absolute top-4 right-4 z-10 flex flex-col items-center gap-2 text-white">
+    <div className="select-none absolute top-4 right-4 z-10 flex flex-col gap-2 text-white">
       {/* 목숨 표시 */}
-      <div className="flex gap-1 text-3xl md:text-4xl">
+      <div className="gap-1 text-3xl md:text-4xl" style={{ display: 'flex', justifyContent: 'flex-end' }}>
         {Array.from({ length: extraLife }).map((_, i) => (
           <span key={i}>❤️</span>
         ))}
