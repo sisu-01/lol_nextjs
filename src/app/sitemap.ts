30f1226e6af404ad25fcb8a35d5fdc72c8c8a5ec
sisu-01 <tesv010615@naver.com>
2026-03-06 23:50:30 +0900

    sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,46 +1,30 @@
 // import { rolesData } from '@/data/data'
 // import { postsData } from '@/data/posts';
-// import type { MetadataRoute } from 'next'
- 
-// export default function sitemap(): MetadataRoute.Sitemap {
-//   const gameUrls: MetadataRoute.Sitemap = rolesData.map(role => ({
-//     url: `https://www.lol-updown.com/game/${role.id}`,
-//     lastModified: new Date(),
-//     changeFrequency: 'yearly',
-//     priority: 0.5
-//   }));
-
-//   const postUrls: MetadataRoute.Sitemap = postsData.map(post => ({
-//     url: `https://www.lol-updown.com/posts/${post.id}`,
-//     lastModified: post.date,
-//     changeFrequency: 'yearly',
-//     priority: 0.7
-//   }));
-
-//   const routes = ['guide', 'news', 'posts', 'privacy', 'terms', 'contact', 'about']
-//   const urls: MetadataRoute.Sitemap = routes.map(route => ({
-//     url: `https://www.lol-updown.com/${route}`,
-//     lastModified: new Date(),
-//     changeFrequency: 'weekly',
-//     priority: 0.7
-//   }));
-
-//   return [
-//     {
-//       url: 'https://www.lol-updown.com',
-//       lastModified: new Date(),
-//       changeFrequency: 'monthly',
-//       priority: 1,
-//     },
-//     ...urls,
-//     ...gameUrls,
-//     ...postUrls
-//   ]
-// }
-
 import type { MetadataRoute } from 'next'
-
+ 
 export default function sitemap(): MetadataRoute.Sitemap {
+  // const gameUrls: MetadataRoute.Sitemap = rolesData.map(role => ({
+  //   url: `https://www.lol-updown.com/game/${role.id}`,
+  //   lastModified: new Date(),
+  //   changeFrequency: 'yearly',
+  //   priority: 0.5
+  // }));
+
+  // const postUrls: MetadataRoute.Sitemap = postsData.map(post => ({
+  //   url: `https://www.lol-updown.com/posts/${post.id}`,
+  //   lastModified: post.date,
+  //   changeFrequency: 'yearly',
+  //   priority: 0.7
+  // }));
+
+  // const routes = ['guide', 'news', 'posts', 'privacy', 'terms', 'contact', 'about']
+  // const urls: MetadataRoute.Sitemap = routes.map(route => ({
+  //   url: `https://www.lol-updown.com/${route}`,
+  //   lastModified: new Date(),
+  //   changeFrequency: 'weekly',
+  //   priority: 0.7
+  // }));
+
   return [
     {
       url: 'https://www.lol-updown.com',
@@ -48,6 +32,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 1,
     },
-    // 나머지 gameUrls, postUrls, routes는 모두 삭제하거나 주석 처리
+    // ...urls,
+    // ...gameUrls,
+    // ...postUrls
   ]
 }
